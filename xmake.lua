b2400e9c681be4e565705302022817c878a29a0e
allen <ivan_allen@163.com>
2024-09-18 22:30:54 +0800

    lock 3rd version

--- xmake.lua
+++ xmake.lua
@@ -15,24 +15,22 @@ if is_mode("debug") then
     add_ldflags("-fsanitize=address")
 end
 
-add_requires("protobuf-cpp 3.19.4", {configs = {zlib = true}})
-add_requireconfs("**.protobuf-cpp", {version = "3.19.4", override=true, configs = {zlib = true}})
-
-add_requires("brpc")
+add_requires("argparse 3.1")
+add_requires("brpc 1.10.0")
+add_requires("boost 1.81.0")
 add_requires("fmt 10.1.1", {configs = {header_only = true}})
-add_requireconfs("**.fmt", {override=true, version="10.1.1", configs = {header_only = true}})
-
+add_requires("gtest 1.12.1")
+add_requires("nlohmann_json v3.11.3")
+add_requires("opentelemetry-cpp 1.16.1")
+add_requires("protobuf-cpp 3.19.4", {configs = {zlib = true}})
 add_requires("spdlog v1.14.1", {configs = {fmt_external = true, header_only = true}})
+add_requires("uuid_v4 1.0.0")
+
+add_requireconfs("**.boost", {override=true, version="1.81.0"})
+add_requireconfs("**.fmt", {override=true, version="10.1.1", configs = {header_only = true}})
+add_requireconfs("**.protobuf-cpp", {version = "3.19.4", override=true, configs = {zlib = true}})
 add_requireconfs("**.spdlog", {override=true, version="v1.14.1"})
 
-add_requires("boost 1.81.0")
-add_requireconfs("**.boost", {override=true, version="1.81.0"})
-
-add_requires("gtest")
-add_requires("argparse")
-add_requires("nlohmann_json")
-add_requires("opentelemetry-cpp")
-add_requires("uuid_v4")
 
 add_includedirs("include")
 add_includedirs("src")
