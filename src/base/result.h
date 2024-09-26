b92314442c429f24fbe557fdde6c606207dc38ee
allen <ivan_allen@163.com>
2024-09-26 09:32:26 +0800

    rusty result

--- src/base/result.h
+++ src/base/result.h
@@ -7,6 +7,163 @@
 #include <boost/stacktrace.hpp>
 
 namespace pain {
+
+// rusty style Result
+namespace rust {
+template <typename T, typename E>
+class [[nodiscard]] Result {
+public:
+    struct Ok {
+        T value;
+    };
+    struct Err {
+        E value;
+    };
+
+    constexpr Result(Ok v) : _value(std::move(v)) {}
+    constexpr Result(Err e) : _value(std::move(e)) {}
+
+    constexpr Result(Result&& other) noexcept = default;
+    constexpr Result& operator=(Result&& other) noexcept = default;
+
+    constexpr T& value() & noexcept {
+        if (is_ok()) {
+            return std::get<Ok>(_value).value;
+        } else {
+            std::cerr << "value error" << std::endl;
+            std::cerr << "Backtrace: " << std::endl;
+            std::cerr << boost::stacktrace::stacktrace() << std::endl;
+            abort();
+        }
+    }
+
+    constexpr const T& value() const& noexcept {
+        if (is_ok()) {
+            return std::get<Ok>(_value).value;
+        } else {
+            std::cerr << "value error" << std::endl;
+            std::cerr << "Backtrace: " << std::endl;
+            std::cerr << boost::stacktrace::stacktrace() << std::endl;
+            abort();
+        }
+    }
+
+    template <typename U>
+    constexpr T value_or(U&& v) & noexcept {
+        if (is_ok()) {
+            return std::get<Ok>(_value).value;
+        } else {
+            return std::forward<U>(v);
+        }
+    }
+
+    constexpr E& err() & noexcept {
+        if (!is_ok()) {
+            return std::get<Err>(_value).value;
+        } else {
+            std::cerr << "err error" << std::endl;
+            std::cerr << "Backtrace: " << std::endl;
+            std::cerr << boost::stacktrace::stacktrace() << std::endl;
+            abort();
+        }
+    }
+
+    constexpr const E& err() const& noexcept {
+        if (!is_ok()) {
+            return std::get<Err>(_value).value;
+        } else {
+            std::cerr << "err error" << std::endl;
+            std::cerr << "Backtrace: " << std::endl;
+            std::cerr << boost::stacktrace::stacktrace() << std::endl;
+            abort();
+        }
+    }
+
+    constexpr T unwrap() && noexcept {
+        if (is_ok()) {
+            return std::get<Ok>(std::move(_value)).value;
+        } else {
+            std::cerr << "unwrap error" << std::endl;
+            std::cerr << "Backtrace: " << std::endl;
+            std::cerr << boost::stacktrace::stacktrace() << std::endl;
+            abort();
+        }
+    }
+
+    constexpr T unwrap_or(T v) && noexcept {
+        if (is_ok()) {
+            return std::get<Ok>(std::move(_value)).value;
+        } else {
+            return v;
+        }
+    }
+
+    constexpr T expect(const char* msg) && noexcept {
+        if (is_ok()) {
+            return std::get<Ok>(std::move(_value)).value;
+        } else {
+            std::cerr << msg << std::endl;
+            std::cerr << "Backtrace: " << std::endl;
+            std::cerr << boost::stacktrace::stacktrace() << std::endl;
+            abort();
+        }
+    }
+
+    constexpr E unwrap_err() && noexcept {
+        if (!is_ok()) {
+            return std::get<Err>(std::move(_value)).value;
+        } else {
+            std::cerr << "err error" << std::endl;
+            std::cerr << "Backtrace: " << std::endl;
+            std::cerr << boost::stacktrace::stacktrace() << std::endl;
+            abort();
+        }
+    }
+
+    template <typename U, typename F>
+    constexpr Result<U, E> map(F&& f) && {
+        if (is_ok()) {
+            return Result<U, E>(
+                typename Result<U, E>::Ok(std::invoke(std::forward<F>(f), std::get<Ok>(std::move(_value)).value)));
+        } else {
+            return Result<U, E>(typename Result<U, E>::Err(std::get<Err>(std::move(_value)).value));
+        }
+    }
+
+    template <typename U, typename F>
+    constexpr Result<U, E> map_or(U&& u, F&& f) && {
+        if (is_ok()) {
+            return Result<U, E>(
+                typename Result<U, E>::Ok(std::invoke(std::forward<F>(f), std::get<Ok>(std::move(_value)).value)));
+        } else {
+            return Result<U, E>(typename Result<U, E>::Ok(std::forward<U>(u)));
+        }
+    }
+
+    constexpr bool is_ok() const {
+        return std::holds_alternative<Ok>(_value);
+    }
+    constexpr bool is_err() const {
+        return std::holds_alternative<Err>(_value);
+    }
+
+private:
+    std::variant<Ok, Err> _value;
+};
+
+template <typename T, typename E>
+constexpr Result<T, E> Ok(T v) {
+    return Result<T, E>(typename Result<T, E>::Ok{std::move(v)});
+}
+
+template <typename T, typename E>
+constexpr Result<T, E> Err(E e) {
+    return Result<T, E>(typename Result<T, E>::Err{std::move(e)});
+}
+} // namespace rust
+} // namespace pain
+
+namespace pain {
 template <typename T, typename E>
     requires(!std::is_same_v<T, E>)
 class [[nodiscard]] Result {
---
+++ src/base/test/test_rusty_result.cc
@@ -0,0 +1,78 @@
+#include <gtest/gtest.h>
+#include <system_error>
+#include "base/result.h"
+
+using namespace pain::rust;
+
+TEST(Result, ok) {
+    auto r = Ok<int, std::error_code>(42);
+    ASSERT_EQ(r.is_ok(), true);
+    ASSERT_EQ(r.is_err(), false);
+    EXPECT_EQ(r.value(), 42);
+    EXPECT_EQ(std::move(r).unwrap(), 42);
+}
+
+TEST(Result, err) {
+    auto expect = std::make_error_code(std::errc::invalid_argument);
+    auto r = Err<int, std::error_code>(expect);
+    ASSERT_EQ(r.is_ok(), false);
+    ASSERT_EQ(r.is_err(), true);
+    EXPECT_EQ(r.err(), expect);
+    EXPECT_EQ(std::move(r).unwrap_err(), expect);
+}
+
+TEST(Result, map_ok) {
+    auto r = Ok<int, std::error_code>(42);
+    auto r2 = std::move(r).map<std::string>([](int v) { return std::to_string(v); });
+    ASSERT_EQ(r2.is_ok(), true);
+    ASSERT_EQ(r2.is_err(), false);
+    EXPECT_EQ(r2.value(), "42");
+}
+
+TEST(Result, map_err) {
+    auto expect = std::make_error_code(std::errc::invalid_argument);
+    auto r = Err<int, std::error_code>(expect);
+    auto r2 = std::move(r).map<std::string>([](int v) { return std::to_string(v); });
+    ASSERT_EQ(r2.is_ok(), false);
+    ASSERT_EQ(r2.is_err(), true);
+    EXPECT_EQ(r2.err(), expect);
+    EXPECT_EQ(std::move(r2).unwrap_err(), expect);
+}
+
+TEST(Result, map_or_ok) {
+    auto r = Ok<int, std::error_code>(42);
+    auto r2 = std::move(r).map_or<std::string>("This is a default value", [](int v) { return std::to_string(v); });
+    ASSERT_EQ(r2.is_ok(), true);
+    ASSERT_EQ(r2.is_err(), false);
+    EXPECT_EQ(r2.value(), "42");
+}
+
+TEST(Result, map_or_err) {
+    auto expect = std::make_error_code(std::errc::invalid_argument);
+    auto r = Err<int, std::error_code>(expect);
+    auto r2 = std::move(r).map_or<std::string>("This is a default value", [](int v) { return std::to_string(v); });
+    ASSERT_EQ(r2.is_ok(), true);
+    ASSERT_EQ(r2.is_err(), false);
+    EXPECT_EQ(r2.value(), "This is a default value");
+}
+
+Result<int, std::error_code> divide(int a, int b) {
+    if (b == 0) {
+        return Err<int, std::error_code>(std::make_error_code(std::errc::invalid_argument));
+    }
+    return Ok<int, std::error_code>(a / b);
+}
+
+TEST(Result, divide_ok) {
+    auto r = divide(99, 3).map<std::string>([](int v) { return std::to_string(v); });
+    ASSERT_EQ(r.is_ok(), true);
+    ASSERT_EQ(r.is_err(), false);
+    EXPECT_EQ(r.value(), "33");
+}
+
+TEST(Result, divide_err) {
+    auto r = divide(99, 0).map<std::string>([](int v) { return std::to_string(v); });
+    ASSERT_EQ(r.is_ok(), false);
+    ASSERT_EQ(r.is_err(), true);
+    EXPECT_EQ(r.err(), std::make_error_code(std::errc::invalid_argument));
+}
